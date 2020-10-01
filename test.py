010e42a939717e253c2ea620c9727dbed7b34361
Achuet414 <32358392+Achuet414@users.noreply.github.com>
2020-10-01 13:09:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 """
-#This module contains shared fixtures for web UI tests.
+This module contains shared fixtures for web UI tests.
+This module contains shared fixtures for web UI tests pull test
 
 """
 
